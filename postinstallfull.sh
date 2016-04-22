39d88c45d2e928c8f62b10b4e16d5f15c3d597f0
Terry Jones <tjones@arataki.school.nz>
2016-04-23 11:44:26 +1200

    Update postinstallfull.sh

--- postinstallfull.sh
+++ postinstallfull.sh
@@ -12,16 +12,16 @@ mkdir /home/$USER/Desktop/Room14
 # Updates the coding workstation and installs a set of games. Also removes unneeded software.
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y git-all minetest pingus hedgewars vlc supertuxkart nexuiz 0ad preload supertux openarena torcs sauerbraten
+sudo apt-get install -y minetest pingus hedgewars vlc supertuxkart nexuiz 0ad preload supertux openarena torcs sauerbraten
 sudo apt-get remove thunderbird hexchat pidgin transmission-gtk banshee xfburn
 # Copies a custom configured Firefox config file and deletes old one. 
-wget "https://www.dropbox.com/s/yj857jcwjvxniqc/syspref.js?dl=0"
-mv syspref.js?dl=0 syspref.js
+git clone https://github.com/Arataki-Coders/initscript.git /home/$USER/Desktop/git
+cd /home/$USER/Desktop/git
 sudo rm -f /etc/firefox/syspref.js
 sudo cp syspref.js /etc/firefox/
 # Refresh current user so config changes take effect.
 sudo sed -i -e '/startup.homepage/d' /home/*/.mozilla/firefox/*/prefs.js
-sudo rm -f syspref.js
+rm -rf /home/$USER/Desktop/git/
 echo "--------------------------------------------"
 echo "Script complete...."
 echo "You can now delete the postinstallfull.sh file"
